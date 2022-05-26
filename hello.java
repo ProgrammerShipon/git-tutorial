ef6fa94925464ded6272fff77c2e03008bba13b3
ShiponHossenRaju <msshipon234@gmail.com>
2022-05-26 15:34:35 +0600

    Added a new method called display

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class Hello {
   public static void main(string[] args) {
-    System.out.printIn("Vs Code is Awesome")
+    System.out.printIn("Vs Code is Awesome");
+
+    greeting(" Programmer Shipon");
+  }
+
+  public static void greeting(String name) {
+    System.out.printIn("Welcome" + name);
   }
 }
