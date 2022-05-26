ee479bf7a972e4e9e09648ece4e999a1bb93ff45
ShiponHossenRaju <msshipon234@gmail.com>
2022-05-26 15:51:54 +0600

    Change greeting method hello.java

--- hello.java
+++ hello.java
@@ -2,10 +2,10 @@ public class Hello {
   public static void main(string[] args) {
     System.out.printIn("Vs Code is Awesome");
 
-    greeting(" Programmer Shipon");
+    greeting("Git");
   }
 
   public static void greeting(String name) {
-    System.out.printIn("Welcome" + name);
+    System.out.printIn("Bye Bye" + name);
   }
 }
